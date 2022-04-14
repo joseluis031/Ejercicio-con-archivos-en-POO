bc4674fe7437708ac33a9e4353a2361ab4479763
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-14 17:55:12 +0200

    tengo que inventarme otro metodo que funcione

--- Clases/archivo.py
+++ Clases/archivo.py
@@ -2,7 +2,7 @@ import csv
 
 class archivos1:
     
-    def __init__(self,lista,):
+    def __init__(self,lista):
         self.lista = lista
         
         
@@ -17,8 +17,31 @@ class archivos1:
                 Asistencia = i[2]
                 Practicas = i[7]
                 self.lista.append(i)
+                
                 print(f"{Apellidos} con {Asistencia} de asistencia ha obetenido unas calificaciones de:" 
                     f"\n Nota del parcial1: {Nota_parcial1} \n Nota del parcial2: {Nota_parcial2} \n Nota en practicas: {Practicas}\n")
+                
+class archivos2:
+    def __init__(self,lista):
+        archivos1.__init__(self,lista)
+        
+    def lectura2(self):
+        with open('calificaciones.csv') as self.File:
+            reader = csv.reader(self.File, delimiter=';')
+            next(reader, None)
+            for i in reader:
+                Apellidos = i[0]
+                Nota_parcial1 = i[3]
+                Nota_parcial2 = i[4]
+                Asistencia = i[2]
+                Practicas = i[7]
+                Nota_final = i[3] + 0.3 * i[4] + 0.4 * i[7]
+                self.lista.append(i)
+                
+                print(f"{Apellidos} con {Asistencia} de asistencia ha obetenido unas calificaciones de:" 
+                    f"\n Nota del parcial1: {Nota_parcial1} \n Nota del parcial2: {Nota_parcial2} \n Nota en practicas: {Practicas}\n"
+                    f"Su nota final es: {Nota_final}")
+
 lista = []
-dos = archivos1(lista)
-dos.lectura()
+dos = archivos2(lista)
+dos.lectura2()
