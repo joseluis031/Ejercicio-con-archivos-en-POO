ab1665f3b84c1c3df3036b7d3a4bd1e0877e6eca
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-14 16:32:03 +0200

    corrijo error solo me falta ordenacion apellidos

--- Clases/archivo.py
+++ Clases/archivo.py
@@ -1,19 +1,24 @@
 import csv
+
 class archivos1:
     
-    def __init__(self,File):
-        self.File = File
+    def __init__(self,lista,):
+        self.lista = lista
+        
         
     def lectura(self):
         with open('calificaciones.csv') as self.File:
             reader = csv.reader(self.File, delimiter=';')
             next(reader, None)
-        lista = []
-        for i in reader:
-            Apellidos = i[0]
-            Nota_parcial1 = i[3]
-            Nota_parcial2 = i[4]
-            Asistencia = i[2]
-            Practicas = i[7]
-            lista.append(i)
-            print(f"{Apellidos} con {Asistencia} % de asistencia ha obetenido unas calificaciones de: \n Nota del parcial1: {Nota_parcial1} \n Nota del parcial2: {Nota_parcial2} \n Nota en practicas: {Practicas}")
+            for i in reader:
+                Apellidos = i[0]
+                Nota_parcial1 = i[3]
+                Nota_parcial2 = i[4]
+                Asistencia = i[2]
+                Practicas = i[7]
+                self.lista.append(i)
+                print(f"{Apellidos} con {Asistencia} de asistencia ha obetenido unas calificaciones de:" 
+                    f"\n Nota del parcial1: {Nota_parcial1} \n Nota del parcial2: {Nota_parcial2} \n Nota en practicas: {Practicas}\n")
+lista = []
+dos = archivos1(lista)
+dos.lectura()
