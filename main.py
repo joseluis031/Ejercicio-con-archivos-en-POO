a2477b941b92f20c584addc9ffeb6fb79a39fc7a
joseluis031 <91721888+joseluis031@users.noreply.github.com>
2022-04-28 23:58:55 +0200

    main

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 if __name__ == "__main__":  
-    main = int(input("Que ejercicio deseas realizar(1,2 o 3):"))
+    main = int(input("Que ejercicio deseas realizar(1 o 2):"))
     if main == 1:
         from Clases.archivo import archivos1
-        
         lista = []
         uno = archivos1(lista)
         uno.lectura()
+    if main ==2:
+        from Clases.archivo import archivos2_1
+        File = "calificaciones.csv"
+        hola = archivos2_1(File)
+        hola.lectura2_1()
